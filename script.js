78742dea69b91a9f1abff6cb9c40a0bb3a0f0d04
Jason Miller <jasonmiller.dev@gmail.com>
2022-08-02 10:49:30 +1000

    Loop3 question

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
-const name = "Mustafa";
+// Loops 3
+// In this final task, you are provided with the following:
+
+// i — starts off with a value of 500; intended to be used as an iterator.
+// para — contains a reference to a paragraph, which will be used to report the results.
+// isPrime() — a function that, when passed a number, returns true if the number is a prime number, and false if not.
+// You need to use a loop to go through the numbers 2 to 500 backwards (1 is not counted as a prime number), and run the provided isPrime() function on them. For each number that isn't a prime number, continue on to the next loop iteration. For each one that is a prime number, add it to the paragraph's textContent along with some kind of separator.
+let i = 500;
 const para = document.createElement("p");
 
-const phonebook = [
-  { name: "Chris", number: "1549" },
-  { name: "Li Kang", number: "9634" },
-  { name: "Anne", number: "9065" },
-  { name: "Francesca", number: "3001" },
-  { name: "Mustafa", number: "6888" },
-  { name: "Tina", number: "4312" },
-  { name: "Bert", number: "7780" },
-  { name: "Jada", number: "2282" },
-];
-
-// Add your code here
-let i = 0;
-while (i < phonebook.length) {
-  if (phonebook[i].name === name) {
-    console.log("A match has been found");
-    para.innerHTML = `${
-      phonebook[i].name + "'s" + " number is " + phonebook[i].number
-    }`;
-    break;
+function isPrime(num) {
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) {
+      return false;
+    }
   }
-  i++;
+
+  return true;
 }
 
+// Add your code here
+while (i != 1) {
+  if (isPrime(i)) {
+    para.textContent += i + ", ";
+  }
+  i--;
+}
 // Don't edit the code below here!
 const section = document.querySelector("section");
 section.appendChild(para);
